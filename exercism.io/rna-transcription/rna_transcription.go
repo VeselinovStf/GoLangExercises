b3d7f4d8cb032f412f5577e76fd47439bb5798a8
Stefan Veselinov <chofexx@gmail.com>
2021-08-01 10:26:06 +0300

    Adding complete solution

--- exercism.io/rna-transcription/rna_transcription.go
+++ exercism.io/rna-transcription/rna_transcription.go
@@ -1,4 +1,21 @@
+// Package strand implements DNA and RNA strands functional methods.
 package strand
 
-func ToRNA(dna string) string {
+var nucleotides = map[rune]string{
+	'G': "C",
+	'C': "G",
+	'T': "A",
+	'A': "U",
+}
+
+// ToRNA converts given DNA strand and returns its RNA complement (per RNA transcription).
+func ToRNA(dna string) (rna string) {
+
+	for _, c := range dna {
+		if val, ok := nucleotides[c]; ok {
+			rna += val
+		}
+	}
+
+	return
 }
