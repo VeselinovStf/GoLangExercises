01492d002233cc4fca6e3aefd0c400ecbce93733
Stefan Veselinov <chofexx@gmail.com>
2021-08-05 07:14:12 +0300

    Adding Completed exercise

--- exercism.io/scale-generator/scale_generator.go
+++ exercism.io/scale-generator/scale_generator.go
@@ -1,91 +1,101 @@
 // Package scale implements music scales generation
 package scale
 
-var sharpNotes = []string{"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"}
-var flatNotes = []string{"A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"}
+import (
+	"strings"
+)
 
-var circleOfFifth = []string{"C", "G", "D", "A", "E", "B", "F#", "e", "b", "f#", "c#", "g#", "d#"}
-var circleOfFourth = []string{"F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb"}
+var (
+	// Chromatic scale with sharps
+	sharpNotes = []string{"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"}
+	// Chromatic scale with flats
+	flatNotes = []string{"A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"}
+	// Sharps circle
+	circleOfFifth = []string{"C", "G", "D", "A", "E", "B", "F#", "e", "b", "f#", "c#", "g#", "d#"}
+	// Flats circle
+	circleOfFourth = []string{"F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb"}
+	// Chromatic steps
+	chromatic = "mmmmmmmmmmmm"
+)
 
 // Scale generate the musical scale by given tonic, or starting note, and a set of intervals,
 // starting with the tonic and following the specified interval pattern
 func Scale(tonic, interval string) (scale []string) {
 
+	// Check for chromatic and set interval
 	if interval == "" {
-		// hromatic
-		interval = "mmmmmmmmmmm"
+		interval = chromatic
 	}
 
-	if ok, _ := contains(circleOfFifth, tonic); ok {
-		// Use sharps
-		start := findNote(sharpNotes, tonic)
-		scale = runScale(sharpNotes, interval, start)
-
-		return
+	// #/b Slices are uppercased - Format the input tonic string
+	ft := strings.Title(tonic)
+	switch {
+	case contains(circleOfFifth, tonic):
+		// Scale with sharp
+		scale = runScale(sharpNotes, interval, ft)
+	case contains(circleOfFourth, tonic):
+		// Scale with flats
+		scale = runScale(flatNotes, interval, ft)
+	default:
+		// C durr / A moll - no ligature
+		scale = runScale(sharpNotes, interval, ft)
 	}
-
-	if ok, _ := contains(circleOfFourth, tonic); ok {
-		// Use flats
-		start := findNote(sharpNotes, tonic)
-		scale = runScale(flatNotes, interval, start)
-
-		return
-	}
-
-	// No armature
-	start := findNote(sharpNotes, tonic)
-	scale = runScale(sharpNotes, interval, start)
 
 	return
 }
 
-func runScale(notes []string, interval string, start int) (r []string) {
+func runScale(notes []string, interval string, tonic string) (r []string) {
+	start := findNote(notes, tonic)
+	// Append Tonic - I
+	r = append(r, notes[start])
 
-	length := len(interval)
-
-	c := 0
-
-	for i := start; i <= length+start; i++ {
-
-		step := interval[c]
+	mem := start
+	for i := start; i < len(interval)+start-1; i++ {
+		// Get current step
+		step := interval[i-start]
 
 		jump := 0
 		switch {
+		// Major sec
 		case step == 'M':
-			jump = 1
-		case step == 'A':
 			jump = 2
+		// Aug
+		case step == 'A':
+			jump = 3
+		// Minor sec
 		default:
-			jump = 0
+			jump = 1
 		}
 
-		if i != start {
-			if i > len(notes)+jump-1 {
-				r = append(r, notes[i%length-1+jump])
-			} else {
-				r = append(r, notes[i+jump])
-			}
-
-			c++
+		bound := mem + jump
+		if bound >= len(notes) {
+			// Spit to start point of notes slice
+			next := bound % len(notes)
+			r = append(r, notes[next])
+			mem = next
 		} else {
-			r = append(r, notes[i])
+			r = append(r, notes[bound])
+			mem = bound
 		}
-
 	}
 
 	return
 }
 
 func findNote(notes []string, s string) (i int) {
-	_, i = contains(notes, s)
+	for i, v := range notes {
+		if v == s {
+			return i
+		}
+	}
 
-	return
+	return 0
 }
 
-func contains(s []string, e string) (r bool, i int) {
-	for i, v := range s {
+func contains(s []string, e string) (r bool) {
+	for _, v := range s {
 		if v == e {
-			return true, i
+			return true
 		}
 	}
 
