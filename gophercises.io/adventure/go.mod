5c19e9986b112c18796ec7d4a084562cd02a7b68
Stefan Veselinov <chofexx@gmail.com>
2021-08-08 14:23:24 +0300

    Initial Project Structure

---
+++ gophercises.io/adventure/go.mod
@@ -0,0 +1,3 @@
+module github.com/veselinovstf/exercises/GoLangExercises/gophercises.io/adventure
+
+go 1.16
---
+++ gophercises.io/adventure/main.go
@@ -0,0 +1,8 @@
+// Package main is main entry point for Adventure App - Choose Your Own Adventure Application
+package main
+
+
+
+func main() {
+
+}
