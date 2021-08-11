8026a01f185afb627de1fb892d3a0aa2722b7462
Stefan Veselinov <chofexx@gmail.com>
2021-08-11 18:39:02 +0300

    Editing Solution

--- exercism.io/difference-of-squares/difference_of_squares.go
+++ exercism.io/difference-of-squares/difference_of_squares.go
@@ -1,27 +1,19 @@
 // Package diffsquares implements calculatios for diffs of squares
 package diffsquares
 
-var sumOfSquares = func(n int)int{
-	return  n*(n+1)*(2*n+1) / 6
-}
-
-var squareOfSum = func(n int) int{
-	eq := n*(n+1) / 2 
-	return eq * eq
-}
-
 // SumOfSquares finds the sum of the squares of the first N natural numbers.
-func SumOfSquares(n int) int{
-	return  sumOfSquares(n)
+func SumOfSquares(n int) int {
+	return n * (n + 1) * (2*n + 1) / 6
 }
 
 // Difference find difference between the square of
 // the sum of the first N natural numbers.
-func Difference(n int) int{
-	return  squareOfSum(n) - sumOfSquares(n)
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
 }
 
 // SquareOfSum find sum of square of the first N natural numbers.
-func SquareOfSum(n int) int{
-	return squareOfSum(n)
+func SquareOfSum(n int) int {
+	sum := n * (n + 1) / 2
+	return sum * sum
 }
